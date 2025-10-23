461633ee0020d72095352f09bbf6dfa63a0d04f8
Vo Van Quyen <vvq0522@gmail.com>
2025-10-24 01:06:01 +0700

    Cài đặt socket.io

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const route = require('./route/index.route')
+const http = require('http')
 
 const app = express()
 const port = 3000
@@ -9,9 +10,19 @@ app.set('view engine', 'pug')
 
 app.use(express.static('public'))
 
+// Socket IO
+const server = http.createServer(app);
+const { Server } = require("socket.io")
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+})
+// End Socket IO
+
 // Route
 route(app)
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
